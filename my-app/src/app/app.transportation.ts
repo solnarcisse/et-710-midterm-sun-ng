bba93a4249e6c3d2eabd3da0ca6ef6bb7e7c4477
solnarcisse <ec2-user@ip-10-16-0-166.ec2.internal>
2022-11-23 17:27:22 +0000

    update

---
+++ my-app/src/app/app.transportation.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.transportation.html',
+  styleUrls: ['./app.transportation.css']
+})
+export class Transportation {
+  title = 'my-app-transportation';
+}
